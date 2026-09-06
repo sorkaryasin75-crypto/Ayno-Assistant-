525a752a2ee20ac0e8f951c2cdcf3ff5e3f520a2
sorkaryasin75-crypto <sorkaryasin75@gmail.com>
2026-09-06 17:54:45 +0600

    Change environment variables to specific values
    Updated environment variable assignments with specific keys.

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ logging.basicConfig(
 logger = logging.getLogger("AynoAssistant")
 
 # --- Environment Variables ---
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-TELEGRAM_GROUP_ID = os.getenv("TELEGRAM_GROUP_ID")
+GROQ_API_KEY = os.getenv("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")
+TELEGRAM_BOT_TOKEN = os.getenv("8680178701:AAEdRETzMseHbOZuMmQFXpR9RemM-CQ8dl0")
+TELEGRAM_GROUP_ID = os.getenv("1002352180501")
 
 # ভ্যারিয়েবল চেকিং (ফেইল সেফ)
 if not all([GROQ_API_KEY, TELEGRAM_BOT_TOKEN, TELEGRAM_GROUP_ID]):
